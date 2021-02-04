7b2a80a4fe8cc8891674dc4af2ebd7989b2faf64
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2021-02-04 12:45:46 +0900

    add HigherOf

--- avl.c
+++ avl.c
@@ -12,3 +12,10 @@ size_t HeightOf(const struct node *p)
 {
 	return p ? p->h : 0;
 }
+
+struct node *HigherOf(const struct node *a, const struct node *b)
+{
+	size_t ah = HeightOf(a), bh = HeightOf(b);
+
+	return (struct node *)(ah > bh ? a : b);
+}
