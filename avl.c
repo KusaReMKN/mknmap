fa23ef85737e5c7ff2eaa5227534aa568e4721ad
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2021-02-04 12:58:24 +0900

    add RotateR, RotateL

--- avl.c
+++ avl.c
@@ -24,3 +24,27 @@ size_t CalcHeight(const struct node *p)
 {
 	return p ? HeightOf(HigherOf(p->l, p->r)) + 1 : 0;
 }
+
+struct node *RotateR(struct node *p)
+{
+	struct node *tmp = p ? p->l : NULL;
+
+	if (!tmp) return p;
+	p->l = tmp->r;
+	tmp->r = p;
+	p->h = CalcHeight(p);
+	tmp->h = CalcHeight(tmp);
+	return tmp;
+}
+
+struct node *RotateL(struct node *p)
+{
+	struct node *tmp = p ? p->r : NULL;
+
+	if (!tmp) return p;
+	p->r = tmp->l;
+	tmp->l = p;
+	p->h = CalcHeight(p);
+	tmp->h = CalcHeight(tmp);
+	return tmp;
+}
